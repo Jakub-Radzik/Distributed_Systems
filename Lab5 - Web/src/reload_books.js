80dfb1abb9fe93c377afe14c291a483e783f6381
Szymon-SR <srszymonsr@gmail.com>
2023-05-09 10:03:32 +0200

    reloading in background

---
+++ Lab5 - Web/src/reload_books.js
@@ -0,0 +1,49 @@
+
+
+function load_books() {
+    
+fetch('http://localhost:57099/Service1.svc/json/books')
+.then(res => {
+    return res.json()
+})
+.then(data => {
+
+    console.log(data)
+    var list = document.getElementById('book_list')
+    list.innerHTML = ""
+    data.forEach(book => {
+      let tr = document.createElement("tr")
+
+      let tr1 = document.createElement("td")
+      tr1.innerText = book.id_element
+      tr.appendChild(tr1)
+
+      let tr2 = document.createElement("td")
+      tr2.innerText = book.name
+      tr.appendChild(tr2)
+
+      let tr3 = document.createElement("td")
+      tr3.innerText = book.author
+      tr.appendChild(tr3)
+
+      let but_td = document.createElement("td")
+      let but_obj = document.createElement("a")
+      let but_i = document.createElement("i")
+      but_obj.className = "btn btn-secondary btn-detail"
+      but_obj.setAttribute("clicked_book_id", book.id_element)
+      but_i.className = "fas fa-angle-double-right"
+      but_obj.innerText = "Details"
+      but_obj.appendChild(but_i)
+
+      but_td.appendChild(but_obj)
+      tr.appendChild(but_td)
+
+      list.appendChild(tr)
+
+
+    })
+
+    var num = document.getElementById('num-of-books')
+    num.innerHTML = `<i class="fas fa-folder"></i> ` + data.length
+})
+}
